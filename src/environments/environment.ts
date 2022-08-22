71cc0fb4a9a760714d5630ebdf3ee472f5f293df
Katherine Colina <katherine@ateneamercantil.com>
2022-08-22 18:10:53 -0400

    front

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  baseUri: 'http://localhost:8080',
-  baseApi: 'http://localhost:8080/api/auth/',
-  baseApiAuth: 'http://localhost:8080/api/auth/',
+  baseUri: 'https://glupdesign.com',
+  baseApi: 'https://glupdesign.com/api/auth/',
+  baseApiAuth: 'https://glupdesign.com/api/auth/',
   keyUsers: 'fwer345g34fk4jh5k34h34jgf4hjfdj345',
-  sessionToken: 'fwefwefwefwewewefrg3454k34k5gger'
+  sessionToken: 'fwefwefwefwewewefrg3454k34k5gger',
 };
